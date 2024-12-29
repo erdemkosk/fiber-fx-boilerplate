680c2eb120127ea778df0a60f3ba21a30eafb585
Erdem Köşk <erdemkosk@gmail.com>
2024-12-30 00:55:09 +0300

    [feat] Restructure main.go dependend modules to bootstrap

--- main.go
+++ main.go
@@ -1,24 +1,10 @@
 package main
 
 import (
-	"github.com/erdemkosk/fiber-fx-boilerplate/src/bootstrap/config"
-	"github.com/erdemkosk/fiber-fx-boilerplate/src/bootstrap/logger"
-	"github.com/erdemkosk/fiber-fx-boilerplate/src/bootstrap/mongodb"
-	"github.com/erdemkosk/fiber-fx-boilerplate/src/bootstrap/server"
+	"github.com/erdemkosk/fiber-fx-boilerplate/src/bootstrap"
 	"go.uber.org/fx"
-	"go.uber.org/fx/fxevent"
-	"go.uber.org/zap"
 )
 
 func main() {
-	app := fx.New(
-		config.Module,
-		logger.Module,
-		fx.WithLogger(func(log *zap.Logger) fxevent.Logger {
-			return &fxevent.NopLogger
-		}),
-		mongodb.Module,
-		server.Module,
-	)
-	app.Run()
+	fx.New(bootstrap.Module).Run()
 }
---
+++ src/bootstrap/fx.go
@@ -0,0 +1,21 @@
+package bootstrap
+
+import (
+	"github.com/erdemkosk/fiber-fx-boilerplate/src/bootstrap/config"
+	"github.com/erdemkosk/fiber-fx-boilerplate/src/bootstrap/logger"
+	"github.com/erdemkosk/fiber-fx-boilerplate/src/bootstrap/mongodb"
+	"github.com/erdemkosk/fiber-fx-boilerplate/src/bootstrap/server"
+	"go.uber.org/fx"
+	"go.uber.org/fx/fxevent"
+	"go.uber.org/zap"
+)
+
+var Module = fx.Options(
+	config.Module,
+	logger.Module,
+	fx.WithLogger(func(log *zap.Logger) fxevent.Logger {
+		return &fxevent.NopLogger
+	}),
+	mongodb.Module,
+	server.Module,
+)
